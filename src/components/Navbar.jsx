c8d8c74d2adb6c5be268a8fb2883f02adf58c742
Muhammad Zain <98042926+zainkhalid10@users.noreply.github.com>
2025-07-11 23:50:08 +0500

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,6 @@ const navLinks = [
   { label: 'Projects', to: 'projects' },
   { label: 'Education', to: 'education' },
   { label: 'Certifications', to: 'certifications' },
-  { label: 'Awards', to: 'awards' },
   { label: 'Contact', to: 'contact' },
 ];
 
